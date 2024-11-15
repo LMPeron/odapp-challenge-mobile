c1c987510a12596ff5dbc9dab9e45a74ce05712d
LMPeron <52711736+LMPeron@users.noreply.github.com>
2024-11-15 07:36:10 -0300

    fix: change env

--- env.js
+++ env.js
@@ -1,18 +1,9 @@
 // LOCAL
 // export default {
-//   APP_URL: 'https://localhost:8080/game/all',
-//   MAIN_APP_URL: 'https://pitstopbet.com/?page=cassino',
-//   VITE_APP_HOST_HTTP: 'https://localhost:3000/api',
+//   API_URL: "http://localhost:3000/api",
 // };
 
 // TEST
 export default {
-  API_URL: "http://localhost:3000/api",
+  API_URL: "https://odapp-challenge-api-e9f8f3c3b49c.herokuapp.com/api",
 };
-
-// PRODUCTION
-// export default {
-//   APP_URL: 'https://vaivaibet.app',
-//   MAIN_APP_URL: 'https://vaivaibet.com',
-//   VITE_APP_HOST_HTTP: 'https://api.vaivaibet.app/api',
-// };
